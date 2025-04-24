b4eb10e3c9aa84208b10a93fee2dc74016d9aa4f
soohwahong <soohwahong@gmail.com>
2025-04-24 10:20:49 -0400

    toggle for reward scheme that uses itilization and max deflection

--- hong_ppo_cantilever.py
+++ hong_ppo_cantilever.py
@@ -150,6 +150,8 @@ class Args:
 
     vis_utilization : bool = False # visualize utilization of each element in the frame
     frame_count_penalty : bool = False # penalize frame count in reward function
+    reward_utilization_scheme : bool = False
+    add_max_deflection_reward : bool = False # add deflection reward to reward_utilization_scheme
 def layer_init(layer, std=np.sqrt(1.0), bias_const=0.0):
     torch.nn.init.orthogonal_(layer.weight, std)
     torch.nn.init.constant_(layer.bias, bias_const)
